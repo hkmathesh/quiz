1ebd267f35663544935e8cd1056735cb8c153611
hkmathesh <hkmathesh@gmail.com>
2025-02-08 20:42:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+// Get values
+
+var btnStartQuiz = document.getElementById('btnStartQuiz')
+
+btnStartQuiz.addEventListener('click', (event) => {
+
+    event.preventDefault(); // Prevent form submission
+
+    var nameRegex = /^[a-zA-Z]+$/
+    var emailRegex = /^[a-zA-Z0-9]+@gmail\.com/
+
+    var sname = document.getElementById('sname').value;
+    var mail = document.getElementById('mail').value;
+
+    var validate = true
+
+    if (nameRegex.test(sname) == false) {
+        alert('Name must be start with alphabets')
+        var validate = false
+    }
+
+    if (nameRegex.test(sname) == true && emailRegex.test(mail) == false) {
+        alert('Email must be the correct format')
+        var validate = false
+    }
+
+    if (validate == true) {
+        sessionStorage.setItem("sname", sname);
+        sessionStorage.setItem("mail", mail);
+
+        window.location.href = "instructions.html";
+    }
+
+})
+
+
+
+
+
